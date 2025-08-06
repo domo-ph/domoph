1f722818033339b20d46cd9f3f6ba6f50dd7a4a8
Vic Jolin <vic@domo.ph>
2025-08-06 10:06:53 +0800

    web server

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  if (req.url.startsWith('/.well-known/')) {
+    const filePath = path.join(__dirname, req.url);
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not Found');
+      } else {
+        res.statusCode = 200;
+        if (req.url.endsWith('apple-app-site-association') || req.url.endsWith('assetlinks.json')) {
+          res.setHeader('Content-Type', 'application/json');
+        } else {
+          res.setHeader('Content-Type', 'text/plain');
+        }
+        res.end(data);
+      }
+    });
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+  }
 });
 
 server.listen(port, hostname, () => {
