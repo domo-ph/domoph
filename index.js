4ebd3042eae62f482dba6719d1412f3d3a27aae1
Vic Jolin <vic@domo.ph>
2025-08-06 10:51:44 +0800

    web server

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
-const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url.startsWith('/.well-known/')) {
-    const filePath = path.join(__dirname, req.url);
+app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
 
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Not Found');
-      } else {
-        res.statusCode = 200;
-        if (req.url.endsWith('apple-app-site-association') || req.url.endsWith('assetlinks.json')) {
-          res.setHeader('Content-Type', 'application/json');
-        } else {
-          res.setHeader('Content-Type', 'text/plain');
-        }
-        res.end(data);
-      }
-    });
-  } else {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-  }
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
